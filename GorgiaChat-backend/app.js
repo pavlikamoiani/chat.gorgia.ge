7ace712cf912521e836bd76ce310b4833be281ef
Pavlik Amoiani <p.amoiani@gorgia.ge>
2025-08-04 14:15:02 +0400

    Add bcrypt, dotenv, jsonwebtoken, and mysql2 dependencies; remove register.js file

--- GorgiaChat-backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-const registerRoute = require('./register');
-app.use('/api', registerRoute);
-
-module.exports = app;
